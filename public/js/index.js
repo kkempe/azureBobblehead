38ea4558c3e30eb41a2cb1523cb9a137aa952977
peteroden <pete.roden@live.com>
2016-02-27 00:21:27 -0500

    few more changes to the layout

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
-var video = document.querySelector("#videoElement");
+var video = document.querySelector("#video");
  
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
  
@@ -11,5 +11,27 @@ function handleVideo(stream) {
 }
  
 function videoError(e) {
-    // do something
+    console.log(e);
 }
+
+//var video = document.querySelector('video');
+var canvas = document.querySelector('#canvas');
+var ctx = canvas.getContext('2d');
+var localMediaStream = null;
+
+function snapshot() {
+    if (localMediaStream) {
+        ctx.drawImage(video, 0, 0);
+        document.querySelector('#image').src = canvas.toDataURL('image/png');
+        document.querySelector('#video').style.display = "none";
+        document.querySelector('#image').style.display = "inline";
+    }
+}
+
+  video.addEventListener('click', snapshot, false);
+  
+  // Not showing vendor prefixes or code that works cross-browser.
+  navigator.getUserMedia({video: true}, function(stream) {
+    video.src = window.URL.createObjectURL(stream);
+    localMediaStream = stream;
+  }, videoError);
